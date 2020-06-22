4c1ec26125d2054df96d5a6dd010ae9ee142aa97
Smitesh Patel <smit9612@gmail.com>
2020-06-21 21:23:51 -0400

    Initial Commit

---
+++ MVVMRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMRxSwift
+//
+//  Created by smitesh patel on 2020-06-21.
+//  Copyright © 2020 Smitesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
